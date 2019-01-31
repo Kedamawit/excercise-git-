20bf2d6babe63243af84752874992fdd89ee03ea
kedamawit <habteked@grinnell.edu>
2019-01-31 17:12:43 -0600

    Practice using eclipse & git

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+import java.io.PrintWriter;
+
+public class HelloWorld{
+
+
+	/**
+	 * A simple introductory Java class.
+	 */
+	  /**
+	   * Print a silly message.
+	 * @return 
+	 * @return 
+	   */
+	  public static void main(String[] args) throws Exception {
+	    PrintWriter pen = new PrintWriter(System.out, true);
+	    pen.println("I am the 1337 h4x0r. Phear me!");
+	    pen.flush();
+	  } // main(String[])
+	}
